37c992551a2bfe235650145692ab586353cdee28
dldesara <79813895+dldesara@users.noreply.github.com>
2023-11-15 16:33:07 -0800

    Add files via upload

---
+++ kill_demo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"time"
+)
+
+func main() {
+	pid := os.Getpid()
+	fmt.Println("Current process PID:", pid)
+
+	err := syscall.Kill(pid, syscall.SIGTERM)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("Process terminated successfully")
+}
